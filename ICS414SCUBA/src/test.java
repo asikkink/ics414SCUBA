1bbc785533539359aadedf073abec31b82fd4e08
Victor Gong <vgong24@gmail.com>
2014-10-23 19:49:08 -1000

    utter failure
    as excepted

--- ICS414SCUBA/src/test.java
+++ ICS414SCUBA/src/test.java
@@ -1,10 +1,35 @@
 //test file
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class test{
-	
 	public static void main(String[] args) {
+		Connection conn = null;
+
 	
 		System.out.println("Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11");
-	
+		try{
+			
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+		}catch (Exception ex){
+			System.out.println("Broken");
+			
+		}
+		try{
+			conn =
+					DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=password");
+			System.out.println("It works");
+		}catch(SQLException ex){
+			System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+			System.out.println("Still doesnt work");
+		}
+		
+		
 	}
 	
 }
