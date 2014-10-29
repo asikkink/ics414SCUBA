482f7bb9455c0109100158b2791d974e71d904cb
Victor Gong <vgong24@gmail.com>
2014-10-28 21:58:21 -1000

    Created Test case for getting pressure group
    wrote function to display the input and table row for getPressureGroup

--- ICS414SCUBA/src/DiveDB.java
+++ ICS414SCUBA/src/DiveDB.java
@@ -1,7 +1,6 @@
 import java.sql.*;
 import java.util.Properties;
 
-
 public class DiveDB {
 	private final String userName = "bigoli";
 	private final String password = "pasta";
@@ -9,9 +8,10 @@ public class DiveDB {
 	private final int portNumber = 3306;
 	private final String dbName = "dive_table";
 	private static Connection conn = null;
-	
+
 	/**
 	 * Constructor
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
@@ -23,8 +23,8 @@ public class DiveDB {
 			e.printStackTrace();
 		}
 	}
-	
-	//Create connection to database
+
+	// Create connection to database
 	public Connection getConnection() throws SQLException {
 		Connection conn = null;
 		Properties connectionProps = new Properties();
@@ -36,42 +36,46 @@ public class DiveDB {
 
 		return conn;
 	}
-	//Close connection to database
-	public void closeConnection(){
+
+	// Close connection to database
+	public void closeConnection() {
 		try {
 			conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/**
 	 * Get pressure group by depth and time
 	 */
-	public static String getPressureGroup(int depth, int time){
+	public static String getPressureGroup(int depth, int time) {
 		String pGroup = "";
-		//select the first number that is both greater or equal to depth and time
+		// select the first number that is both greater or equal to depth and
+		// time
 		PreparedStatement prestmt = null;
-		
+
 		try {
-			prestmt = conn.prepareStatement("select * from bottom_time where depth>=? and time>=?");
+			prestmt = conn
+					.prepareStatement("select * from bottom_time where depth>=? and time>=?");
 			prestmt.setInt(1, depth);
 			prestmt.setInt(2, time);
-			
+
 			ResultSet rs = prestmt.executeQuery();
-			System.out.println(rs.getRow());
-			rs.next();
-			System.out.println(rs.getRow()+" " + rs.getInt("depth")+" "+ rs.getInt("time"));
-			pGroup = rs.getString("pressureG");
-		
-		
+			if (rs.next()) {
+
+				System.out.println(rs.getRow() + " " + rs.getInt("depth") + " "
+						+ rs.getInt("time"));
+				pGroup = rs.getString("pressureG");
+			}
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			if(prestmt !=null){
+		} finally {
+			if (prestmt != null) {
 				try {
 					prestmt.close();
 				} catch (SQLException e) {
@@ -80,13 +84,47 @@ public class DiveDB {
 				}
 			}
 		}
-		
-		
-		
-		
-		return pGroup;
-		
+		return "none";
 	}
-	
+
+	/**
+	 * Get Result set
+	 * 
+	 * For testing purposes View the entire row of the getPressureGroup function
+	 */
+	public static String getBottomTimeRow(int depth, int time) {
+		String row = "current depth: " + depth + " current time: " + time + " ";
+		PreparedStatement prestmt = null;
+		try {
+			prestmt = conn
+					.prepareStatement("select * from bottom_time where depth>=? and time>=?");
+			prestmt.setInt(1, depth);
+			prestmt.setInt(2, time);
+
+			ResultSet rs = prestmt.executeQuery();
+			if (rs.next()) {
+
+				row += "db depth: " + rs.getInt("depth") + " db time: "
+						+ rs.getInt("time") + " pressure group: "+ rs.getString("PressureG");
+				return row;
+			}else{
+				row += "Could not find";
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (prestmt != null) {
+				try {
+					prestmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return row;
+	}
 
 }
