d3aed217bca1b8759341168a5dd0c1dfbaf9abe3
Victor Gong <vgong24@gmail.com>
2014-11-03 21:45:25 -1000

    GuiApp1 Interface Example
    it has no comments. Refer here
    http://java.about.com/od/creatinguserinterfaces/ss/Example-Java-Code-For-Building-A-Simple-Gui-Application.htm

--- ICS414SCUBA/src/DiveInterface.java
+++ ICS414SCUBA/src/DiveInterface.java
@@ -1,3 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public interface DiveInterface {
 
---
+++ ICS414SCUBA/src/GuiApp1.java
@@ -0,0 +1,54 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GuiApp1 {
+
+	public static void main(String[] args) {
+		new GuiApp1();
+	}
+
+	public GuiApp1() {
+		JFrame guiFrame = new JFrame();
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Example GUI");
+		guiFrame.setSize(300, 250);
+		guiFrame.setLocationRelativeTo(null);
+		String[] fruitOptions = { "Apple", "Apricot", "Banana", "Cherry",
+				"Date", "Kiwi", "Orange", "Pear", "Strawberry" };
+		String[] vegOptions = { "Asparagus", "Beans", "Broccoli", "Cabbage",
+				"Carrot", "Celery", "Cucumber", "Leek", "Mushroom", "Pepper",
+				"Radish", "Shallot", "Spinach", "Swede", "Turnip" };
+		final JPanel comboPanel = new JPanel();
+		JLabel comboLbl = new JLabel("Fruits:");
+		JComboBox fruits = new JComboBox(fruitOptions);
+		comboPanel.add(comboLbl);
+		comboPanel.add(fruits);
+
+		final JPanel listPanel = new JPanel();
+		listPanel.setVisible(false);
+		JLabel listLbl = new JLabel("Vegetables:");
+		JList vegs = new JList(vegOptions);
+		vegs.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		listPanel.add(listLbl);
+		listPanel.add(vegs);
+		JButton vegFruitBut = new JButton("Fruit or Veg");
+		vegFruitBut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				listPanel.setVisible(!listPanel.isVisible());
+				comboPanel.setVisible(!comboPanel.isVisible());
+			}
+		});
+		guiFrame.add(comboPanel, BorderLayout.NORTH);
+		guiFrame.add(listPanel, BorderLayout.CENTER);
+		guiFrame.add(vegFruitBut, BorderLayout.SOUTH);
+		guiFrame.setVisible(true);
+
+	}
+}
