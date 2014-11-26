22e7c1aee1baf0da13c81983d9cbd3b3256ce9ed
Victor Gong <vgong24@gmail.com>
2014-11-25 18:33:59 -1000

    First test case
    add first dive

--- ICS414SCUBA v2/testCases.php
+++ ICS414SCUBA v2/testCases.php
@@ -24,7 +24,16 @@ else{
 
 ///this will handle everything. Create all variable 
 function testFunctions($db, $POST){
-	//
+//Case 1: Adding 3 Dives
+	//Add first dive of 60ft for 11 min and surfint of 47
+	$depth = &$POST['depth_select'];
+	$bottomTime = &$POST['bottom_time_select'];
+	$surfaceInt = &$POST['surface_int_select'];
+	$depth = 60;
+	$bottomTime = 11;
+	$surfaceInt = 47;
+	
+	addDive($db, $POST);
 	
 	
 }
