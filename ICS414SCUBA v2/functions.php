7c5962a074636d396b36de064c979a76de56d454
Victor Gong <vgong24@gmail.com>
2014-11-30 16:47:21 -1000

    Condense everything before I break something
    Added some documentation and took old code out

--- ICS414SCUBA v2/functions.php
+++ ICS414SCUBA v2/functions.php
@@ -24,7 +24,9 @@ function initialize($db,$POST){
 		//send error
 	}else echo "No rows returned by database.";
 }
-//if a specific depth has been selected, retrieve the bottom times for that depth
+/**If a specific depth has been selected, retrieve the bottom times for that depth
+*  Field affected: Bottom Time(minutes)
+*/
 function selectDepth($db, $POST){
 	if(isset($POST['depth'])){
 		//set sql statement
@@ -46,7 +48,9 @@ function selectDepth($db, $POST){
 		//send error
 	}else echo "No rows returned by database.";
 }
-//if a specific bottom time has been selected, retrieve the surface intervals for that depth
+/**If a specific bottom time has been selected, retrieve the surface intervals for that depth
+*  Field affected: Surface Interval (minutes)
+*/
 function selectBottomTime($db, $POST){
 	if(isset($_POST['bottom_time'])){
 		//set sql statement
@@ -68,16 +72,18 @@ function selectBottomTime($db, $POST){
 		//send error
 	}else echo "No rows returned by database.";
 }
-//When submit button is pressed, manage current input
+/**When submit button is pressed, manage add dive to dive table
+*  Test: echo's dive row back to client console
+*/
 function addDive($db, $POST){
 	$profileID = 1;
 	$diveNum = getDiveNum($db, $profileID);
 	
-	//get initial PG
+	//get current dive values
 	$initialPG = getInitialPG($db, $profileID, $diveNum-1);
 	$postDivePG = getPostDivePG($db, $POST['depth_select'], $POST['bottom_time_select']);
 	$postSurfacePG = getPostSurfaceIntPG($db, $postDivePG, $POST['surface_int_select']);
-	//insert to table
+	//insert values into table
 	$sql = "INSERT INTO `dives` VALUES ('$profileID', '$diveNum', '$initialPG', '{$POST['depth_select']}', 
 	'{$POST['bottom_time_select']}', '$postDivePG', '{$POST['surface_int_select']}', '$postSurfacePG') ";
 	mysqli_query($db, $sql);
@@ -88,23 +94,25 @@ function addDive($db, $POST){
 	
 }
 
+/**Gets the LASTEST dive number
+*  Used: AddDive will insert the next incremented dive number to dives
+*/
 function getDiveNum($db, $profileID){
 	$sql = "SELECT `dive_num` FROM `dives` WHERE `profile_id` = '$profileID'";
 	
 	if(!$result = mysqli_query($db, $sql)) return "MySQL error: ".mysqli_error($db);
-	
-	//see how many rows were returned
 	$num_dives = mysqli_num_rows($result);
-	
 	//if empty table returned, there is no dive yet
 	if($num_dives == 0) $dive_num = 1; 
-	//else we have the dive nums
 	else {
 		$dive_num = $num_dives + 1;
 	}
 	return $dive_num;
 }
 
+/**Gets the post_surface_int_pg from previous dive
+*  Used: AddDive sets the Initial Pressure group of current dive.
+*/
 function getInitialPG($db, $profileID, $prevDiveNum){
 	//if no previous dives, initialize first one
 	$sql = "SELECT `post_surf_int_pg` FROM `dives` WHERE `profile_id` = '$profileID' AND `dive_num` = '$prevDiveNum'";
@@ -119,11 +127,13 @@ function getInitialPG($db, $profileID, $prevDiveNum){
 		$last_dive = end($ipg);
 		$init_pressure_group = $last_dive;
 	}
-	
 	return $init_pressure_group;	
-	
 }
-
+/**Get the post dive pressure group
+======================================================================
+*  Probably need to include residual time in one of these functions
+======================================================================
+*/
 function getPostDivePG($db, $depth, $time){
 
 	$sql = "SELECT `pressure_group` FROM `bottom_time` WHERE `depth` = '$depth' AND `time` = '$time'";
@@ -135,8 +145,9 @@ function getPostDivePG($db, $depth, $time){
 	}
 	
 }
-
-
+/**Get the post surface interval pressure group
+*
+*/
 function getPostSurfaceIntPG($db, $postDivePG, $surfInt) {
 	
 	$sql = "SELECT `final_pressure_group` FROM `surface_interval` WHERE `init_pressure_group` = '$postDivePG' AND `end_time` = '$surfInt'";
@@ -149,72 +160,6 @@ function getPostSurfaceIntPG($db, $postDivePG, $surfInt) {
 	
 }
 
-
-
-// function getFinalPressureGroup($db){
-	// if(isset($_POST['surface_int_select'])){
-		// //$message = "wrong answer";
-		// //echo "YAY AJAX!!!";
-		
-		// //$sql = "SELECT `final_pressure_group` from `surface_interval` WHERE `init_pressure_group` IN ( SELECT `pressure_group` FROM `bottom_time` WHERE `depth` = '{$_POST['depth_selected']}' AND `time` = '{$_POST['bottom_time']}') ORDER BY `end_time` ASC";
-		// /*
-		// ======================================================================
-		// Properly get the final pressure group using initial pressure group 
-		
-		// ======================================================================
-		// */
-		
-		
-		// $sql = "SELECT `final_pressure_group` FROM `surface_interval` WHERE `end_time` = '{$_POST['surface_int_select']}'";
-		// //see if there is a result when we run the query
-		// if(!$result = mysqli_query($db, $sql)){
-			// //if no result, there was an error
-			// return "MySQL error:".mysqli_error($db);
-		// }
-		// //if there is no mysql error, see if any rows were returned from the query
-		// else if(mysqli_num_rows($result)){
-			// $response = "";
-			// for($i=0;$i<mysqli_num_rows($result);$i++){
-				// $data = mysqli_fetch_assoc($result);
-				// $response = $data;
-			// }
-			// return $response['final_pressure_group'];
-		// }
-	// }
-	// else return "no surface int!!";
-
-// }
-
-// function getPressureGroup($db){
-	// if(isset($_POST['depth_select']) && isset($_POST['bottom_time_select'])){
-		// $depth = $_POST['depth_select'];
-		// $time = $_POST['bottom_time_select'];
-		
-		// //$sql = "SELECT `pressure_group` FROM `bottom_time` WHERE `depth` = '{$_POST['depth_select']}' AND `time` = '{$_POST['bottom_time_select']}'";
-		// $sql = "select `pressure_group` from `bottom_time` where `depth` = '{$_POST['depth_select']}' and `time` = '{$_POST['bottom_time_select']}'";
-		// /*
-		// =============================================================
-		// What we've learned today 11-18 1:22AM
-		// PHP HATES US
-		// Apparently the two queries give different results
-		// ` <- This matters... 2 hours to discover
-		
-		// ==============================================================
-		
-		// */
-		// if(!$result = mysqli_query($db, $sql)){
-			// return "MySQL error:".mysqli_error($db);
-		// }
-		// else if(mysqli_num_rows($result)){
-			// $response = "";
-			// $data = mysqli_fetch_assoc($result);
-			// $response = $data;
-			// return $response['pressure_group'];
-		// }
-		
-	// }
-	// else return "no depth selected!!";
-// }
 
 
 
