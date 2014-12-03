430e7bf5c42cec3a84894fdf29bb6e27c2a1c096
Victor Gong <vgong24@gmail.com>
2014-12-02 23:47:17 -1000

    more diveChart.js changes
    more stuff. (Html error happened)

--- ICS414SCUBA v2/js/diveChart.js
+++ ICS414SCUBA v2/js/diveChart.js
@@ -1,17 +1,24 @@
 
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
+
+// $('#addDive').on('click', function(){
+	// drawChart();
+// });
+
+
+
 function drawChart(){
-	var data = google.visualization.arrayToDataTable([
-	['Director (Year)',  'Depth', 'Stops'],
-	['Alfred Hitchcock (1935)', 55, 15],
-	['Ralph Thomas (1959)',     65,15],
-	['Don Sharp (1978)',        25,15],
-	['Don Sharp (1978)',        5,15],
-	['Don Sharp (1978)',        35,15],
-	['Don Sharp (1978)',       20,15],
-	['James Hawes (2008)',      25,15]
-	]);
+	// var data = google.visualization.arrayToDataTable([
+	// ['Director (Year)',  'Depth', 'Stops'],
+	// ['Alfred Hitchcock (1935)', 55, 15],
+	// ['Ralph Thomas (1959)',     65,15],
+	// ['Don Sharp (1978)',        25,15],
+	// ['Don Sharp (1978)',        5,15],
+	// ['Don Sharp (1978)',        35,15],
+	// ['Don Sharp (1978)',       20,15],
+	// ['James Hawes (2008)',      25,15]
+	// ]);
 	
 	//get dive rows
 	$.ajax({
@@ -20,42 +27,44 @@ function drawChart(){
 		data :{
 			action : 'get_dive_data'
 		},
-		success: function(results){
+success: function(results){
 			var myJson = JSON.parse(results);
-			var time = 0;
+			var time;
 			var depth = 0;
 			// console.log(myJson);
 			// console.log(myJson[0].depth);
 			// console.log(myJson.length);
 			var data = new google.visualization.DataTable();
-			data.addColumn('number', 'Time');
+			data.addColumn('string', 'Time');
 			data.addColumn('number', 'Depth');
 			for(var i = 0; i < myJson.length; i++){
-				time = parseInt(myJson[i].time);
-				depth = myJson[i].depth;
+				time = (myJson[i].time);
+				depth = parseInt(myJson[i].depth);
 				data.addRow(
-					[time, depth]
+				[time, depth]
 				);
 				console.log(myJson[i].depth);
 				console.log(myJson[i].time);
 			}
+			var options = {
+title: 'Dive Profile',
+vAxis: {title: 'Depth of Dive'},
+isStacked: true
+			};
+
+			var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
+
+			chart.draw(data, options);
+			
 			
 		},
-		error: function(){
+error: function(){
 			console.log('failure');
 		}
-	
-	
+		
+		
 	})
 	
 
-	var options = {
-title: 'The decline of \'The 39 Steps\'',
-vAxis: {title: 'Accumulated Rating'},
-isStacked: true
-	};
-
-	var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
-
-	chart.draw(data, options);
+	
 }
