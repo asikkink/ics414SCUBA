95e43d14919ced7a74b1c376948a2b1bb4b4aabc
Victor Gong <vgong24@gmail.com>
2014-12-03 00:27:37 -1000

    changed to columnchart
    going to have double bar graph for dive and surface interval

--- ICS414SCUBA v2/js/diveChart.js
+++ ICS414SCUBA v2/js/diveChart.js
@@ -6,16 +6,6 @@ google.setOnLoadCallback(drawChart);
 
 
 function drawChart(){
-	// var data = google.visualization.arrayToDataTable([
-	// ['Director (Year)',  'Depth', 'Stops'],
-	// ['Alfred Hitchcock (1935)', 55, 15],
-	// ['Ralph Thomas (1959)',     65,15],
-	// ['Don Sharp (1978)',        25,15],
-	// ['Don Sharp (1978)',        5,15],
-	// ['Don Sharp (1978)',        35,15],
-	// ['Don Sharp (1978)',       20,15],
-	// ['James Hawes (2008)',      25,15]
-	// ]);
 	
 	//get dive rows
 	$.ajax({
@@ -49,7 +39,7 @@ vAxis: {title: 'Depth of Dive'},
 isStacked: true
 			};
 
-			var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
+			var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
 
 			chart.draw(data, options);
 			
